11534b8b4ce9e2de5de3fb1b6847229a0b7eedf2
Yi Wang <yi.wang.2005@gmail.com>
2014-03-18 22:13:27 -0700

    Add pub-story.sh and pub.sh.

---
+++ story/pub-story.sh
@@ -0,0 +1,13 @@
+cat <<EOF
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+   <title>分布式机器学习的故事</title>
+   <meta name="author" content="Yi Wang" />
+   <link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link>
+</head>
+<body>
+EOF
+
+markdown $1
---
+++ story/pub.sh
@@ -0,0 +1,1 @@
+for i in *.md; do ./pub-story.sh $i > html/$i.html; done
